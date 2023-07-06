2ee21ce2f302b31599cddfe0d8e40612e6a4d380
devil1321 <s.dominik1321@gmail.com>
2023-07-06 19:46:46 +0200

    update

--- myApp.js
+++ myApp.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+const express = require('express');
+const app = express();
+
+const PORT = 8000
+
+app.get('/', (req, res) =>{
+    console.log('Hello World')
+})
+
+app.listen(PORT, ()=>{
+    console.log('server listening on port' + PORT); 
+})
+
