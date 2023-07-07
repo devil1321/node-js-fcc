853c43b289bf40953e9829a46b6172a5a583e631
devil1321 <s.dominik1321@gmail.com>
2023-07-07 12:50:54 +0200

    mongoose 2

--- myApp.js
+++ myApp.js
@@ -2,53 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
-const middleware = (req,res,next) => {
-    console.log(`${req.method} ${req.path}-${req.ip}`)
-    next()
-}
-
-const timeMiddleware = (req,res,next) =>{
-    req.time = new Date().toString()
-    next()
-}
-
-app.use(bodyParser.urlencoded({ extended: true }))
-
-const path = __dirname + '/views/index.html'
-
-app.use('/public',express.static(__dirname + '/public'))
-
-app.use(middleware)
-
-app.get('/',(req,res)=>{
-    res.sendFile(path)
-})
-
-app.get('/now',timeMiddleware,(req,res)=>{
-    res.json({"time":req.time})
-})
-
-app.get('/json',(req,res)=>{
-    if(!process.env.MESSAGE_STYLE === 'uppercase'){
-        res.json({"message": "Hello json"})
-    }else{
-        res.json({message: "Hello json".toUpperCase()})
-    }
-})
-
-app.get("/:word/echo",(req,res)=>{
-    const word = req.params.word
-    res.json({"word":word})
-})
-
-app.use('/name',(req,res)=>{
-    const query = req.query
-    res.json({...query})
-})
-
-app.post('/name',(req,res)=>{
-    res.json({name:`${req.body.name},${req.body.lastname}`})
-})
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser:true,useUnifiedTopology:true }).then(()=>console.log('DB Connected'))
 
 app.listen(8000,()=>console.log('Express listening on port 8000'))
