52ded0fc3ad47e9de510557af2a640a6feaf04d8
devil1321 <s.dominik1321@gmail.com>
2023-07-07 13:14:23 +0200

    mongoose 5

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const Person = require('../models/Person')
+const Person = require('./models/person.model')
 
 
 mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser:true,useUnifiedTopology:true }).then(()=>console.log('DB Connected'))
@@ -31,6 +31,11 @@ app.post('/add-persons', (req,res)=>{
         favoruiteFoods:['Orange']
     }])
     res.end()
+})
+
+app.get('/find-many', async(req,res)=>{
+    const persons = await Person.find()
+    res.json(persons)
 })
 
 app.listen(8000,()=>console.log('Express listening on port 8000'))
