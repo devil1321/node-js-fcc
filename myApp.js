be2f3f4ffcc95cc441e30dd0c6bd19d773205768
devil1321 <s.dominik1321@gmail.com>
2023-07-07 10:47:42 +0200

    express 3

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ const app = express()
 
 const path = __dirname + '/views/index.html'
 
+app.use('/public',express.static(__dirname + '/public'))
+
 app.get('/',(req,res)=>{
     res.sendFile(path)
 })
