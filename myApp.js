04d731f913f891a96a2b55101d4296b8c82d2085
devil1321 <s.dominik1321@gmail.com>
2023-07-07 10:43:57 +0200

    express 2

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 
+const path = __dirname + '/views/index.html'
 
 app.get('/',(req,res)=>{
-    res.send('Hello Express')
+    res.sendFile(path)
 })
 
 
