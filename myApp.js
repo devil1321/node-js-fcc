9a7f5da65945862e07560231dc00d9c269755cc5
devil1321 <s.dominik1321@gmail.com>
2023-07-07 08:37:00 +0200

    dev

--- myApp.js
+++ myApp.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+
+const PORT = 8000
+
+app.get('/', (req, res) =>{
+    console.log('Hello World')
+    res.send('fcc')
+})
+
+app.listen(PORT, ()=>{
+    console.log('server listening on port' + PORT); 
+})
+
