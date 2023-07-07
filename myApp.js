08f210e541a74860c1a15a8b035474e8ac96f320
devil1321 <s.dominik1321@gmail.com>
2023-07-07 09:07:28 +0200

    lesson 1

--- myApp.js
+++ myApp.js
@@ -1,13 +1,1 @@
-const express = require('express');
-const app = express();
-
-
-app.get('/', (req, res) =>{
-    console.log('Hello World')
-    res.send('fcc')
-})
-
-app.listen(process.env.PORT, ()=>{
-    console.log('server listening on port ' + process.env.PORT); 
-})
-
+console.log('Hello World')
